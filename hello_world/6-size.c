11f4c850f744f1ccd71d7863e76be380119faafc
laet.sz <laetitiasuarez91@gmail.com>
2025-10-21 13:44:34 +0200

    hello_world

---
+++ hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints a specific string using printf
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	-Wall
+	return (0);
+}
