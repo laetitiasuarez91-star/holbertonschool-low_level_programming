6e36ed2153af39852bd81fe14d7ebbe760e81dc9
laet.sz <laetitiasuarez91@gmail.com>
2025-10-21 13:26:38 +0200

    hello_world

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -3,12 +3,12 @@
 /**
  * main - Entry point
  *
- * Description: Prints a specific string using puts
+ * Description: Prints a specific string using printf
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("\"with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
